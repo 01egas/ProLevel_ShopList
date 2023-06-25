a4d3aab734eb19ab6d2d5a6b4b0889e60b5face7
01egas <38017925+01egas@users.noreply.github.com>
2023-06-25 19:11:11 +0300

    add presentation viewmodel

--- app/src/main/java/com/example/prolevel_shoplist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/prolevel_shoplist/presentation/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.prolevel_shoplist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.example.prolevel_shoplist.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.liveDataShopList.observe(this){list ->
+            Log.d("MainActivityList", "MainActivityList ${list.toString()}")
+        }
+        viewModel.getShopList()
     }
 }
